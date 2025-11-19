8446e0326f1004e086e1731171aa3c22a07350e3
RudikMamyan <rudik.mamyan96@gmail.com>
2025-11-19 15:35:11 +0400

    feat: wrap app with React Query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/queryClients'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<div>Home page</div>} />
-      <Route path="/photo/:id" element={<div>Photo details</div>} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+        <Route path="/photo/:id" element={<div>Photo details</div>} />
+      </Routes>
+    </QueryClientProvider>
   )
 }
 
